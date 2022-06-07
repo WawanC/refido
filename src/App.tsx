c288a668961ced3ec995c7d018743a0e6022a349
WawanC <kurniawancristianto@gmail.com>
2022-06-07 11:12:47 +0700

    add typescript css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-function App() {
-  return <h1>Refido</h1>;
-}
+import classes from "./App.module.css";
+
+const App: React.FC = () => {
+  return <h1 className={classes.title}>Refido</h1>;
+};
 
 export default App;
