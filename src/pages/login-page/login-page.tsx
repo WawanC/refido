3a41169e51c60b2a22fb2ffb2e000fa288ff727e
WawanC <kurniawancristianto@gmail.com>
2022-06-08 12:57:48 +0700

    design login page

--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -2,8 +2,21 @@ import classes from "./login-page.module.css";
 
 const LoginPage: React.FC = () => {
   return (
-    <main>
-      <h1>Login Page</h1>
+    <main className={classes.main}>
+      <form className={classes.form}>
+        <h1 className={classes.formTitle}>Sign-In</h1>
+        <div className={classes.inputBox}>
+          <label htmlFor="email">E-Mail</label>
+          <input type="email" id="email" />
+        </div>
+        <div className={classes.inputBox}>
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" />
+        </div>
+        <div className={classes.actionBox}>
+          <button type="submit">Login</button>
+        </div>
+      </form>
     </main>
   );
 };
