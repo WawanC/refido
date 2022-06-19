fd4f95265d6177d67eaaefd6a8d5c845b2948636
WawanC <kurniawancristianto@gmail.com>
2022-06-19 21:10:21 +0700

    connecting create todo to hook

--- src/components/NewTodoModal/NewTodoModal.tsx
+++ src/components/NewTodoModal/NewTodoModal.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { useCreateTodo } from "../../hooks/todo";
 import classes from "./NewTodoModal.module.css";
 
 interface INewTodoModal {
@@ -5,14 +7,41 @@ interface INewTodoModal {
 }
 
 const NewTodoModal: React.FC<INewTodoModal> = (props) => {
+  const [createTodo, createTodoLoading] = useCreateTodo();
+  const [enteredTitle, setEnteredTitle] = useState<string>("");
+
+  const changeEnteredTitleHandler: React.ChangeEventHandler<
+    HTMLInputElement
+  > = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const submitFormHandler: React.FormEventHandler<HTMLFormElement> = async (
+    event
+  ) => {
+    event.preventDefault();
+
+    await createTodo({ title: enteredTitle.trim() });
+
+    setEnteredTitle("");
+    props.onClose();
+  };
+
   return (
     <main className={classes.main}>
       <div className={classes.backdrop} onClick={props.onClose} />
-      <form className={classes.box}>
+      <form className={classes.box} onSubmit={submitFormHandler}>
         <h1 className={classes.title}>Create New Todo</h1>
         <div className={classes.inputBox}>
           <label htmlFor="title">Title</label>
-          <input type="text" id="title" placeholder="Todo Title" />
+          <input
+            type="text"
+            id="title"
+            placeholder="Todo Title"
+            value={enteredTitle}
+            onChange={changeEnteredTitleHandler}
+            required
+          />
         </div>
         <div className={classes.actionsBox}>
           <button type="submit">Add Todo</button>
