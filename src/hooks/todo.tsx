9781444bb56db142b963c585c0ae76c03fa8c5e1
WawanC <kurniawancristianto@gmail.com>
2022-06-21 12:38:42 +0700

    fix netlify

--- src/hooks/todo.tsx
+++ src/hooks/todo.tsx
@@ -1,4 +1,4 @@
-import { off, onValue, push, ref, remove, update } from "firebase/database";
+import { onValue, push, ref, remove, update } from "firebase/database";
 import { useEffect, useState } from "react";
 import { useAuth } from "../contexts/auth";
 import { database } from "../utils/firebase";
